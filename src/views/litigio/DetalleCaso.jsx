402987ae520675748ce9ef46825180e97b962fe9
durquilla-iiresodh <durquilla@iiresodh.org>
2026-05-25 16:45:00 -0600

    Agregar SIPDH a envío de correo además de la dirección contacto@sipdh.com

--- src/views/litigio/DetalleCaso.jsx
+++ src/views/litigio/DetalleCaso.jsx
@@ -592,7 +592,7 @@ export default function DetalleCaso({ caso, onVolver, currentUserEmail, userRole
     const guardarDocumentoEnFirestore = async (pdfNombre = '', pdfUrl = '', storagePath = '') => {
       try {
         const modeloDocumentoMail = {
-          from: 'contacto@sipdh.com', 
+          from: 'SIPDH <contacto@sipdh.com>', 
           to: listaCorreos,
           template: {
             name: 'comunicado_institucional',
