6f4a9a7163ce61a497789b7876f6a4d41d0be35c
durquilla-iiresodh <durquilla@iiresodh.org>
2026-06-11 19:13:03 -0600

    Corrección de logo SIPDH a SPIDH

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -61,7 +61,7 @@ export default function Layout({ children, currentView, setView, userRole }) {
             
             {/* 🚀 LOGO TEXTUAL: Reducción de proporciones en Toolbar */}
             <img 
-              src="/SIPDH-white-26.png" 
+              src="/SPIDH-white-26.png" 
               alt="ISIPDH - Intranet" 
               style={{ 
                 height: '26px', 
